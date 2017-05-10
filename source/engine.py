ea487f1cdb4e83127cc8c80958369100525a1381
mudox <mudoxisme@gmail.com>
2017-05-10 10:35:16 +0800

    Inital commit

---
+++ source/engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Engine:
+
+    """
+    The run loop that drive the whole read -> parse -> format -> output loop
+    """
+
+    def __init__(self):
+
+
+
